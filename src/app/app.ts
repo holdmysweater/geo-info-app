b691134f0b2c0b9b3f9814151dd1bac828c4e07c
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-11 23:05:02 +0300

    feat: import TuiCardLarge in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiCardLarge } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, TuiCardLarge],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
