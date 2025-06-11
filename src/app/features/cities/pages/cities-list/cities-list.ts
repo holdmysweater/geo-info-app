e6f58abc07c18d3a8a07e2755f47e3d792c31ce0
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-11 21:11:17 +0300

    feat: generate component cities page cities-list

---
+++ src/app/features/cities/pages/cities-list/cities-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cities-list',
+  imports: [],
+  templateUrl: './cities-list.html',
+  styleUrl: './cities-list.css'
+})
+export class CitiesList {
+
+}
