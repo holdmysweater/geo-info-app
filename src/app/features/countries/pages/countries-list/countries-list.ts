e447bc8be6f3e871e94237c97c947bd1bed13039
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-11 21:09:40 +0300

    feat: generate component countries page countries-list

---
+++ src/app/features/countries/pages/countries-list/countries-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-list',
+  imports: [],
+  templateUrl: './countries-list.html',
+  styleUrl: './countries-list.css'
+})
+export class CountriesList {
+
+}
