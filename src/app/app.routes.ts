5ba59489137e0cc288cc8322cd3fab491ba4a283
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-11 21:19:09 +0300

    feat: route path to cities

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CountriesList } from './features/countries/pages/countries-list/countries-list';
+import { CitiesList } from './features/cities/pages/cities-list/cities-list';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/countries', pathMatch: 'full' },
   { path: 'countries', component: CountriesList },
+  { path: 'cities', component: CitiesList },
 ];
