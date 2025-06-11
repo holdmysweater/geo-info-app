a5a9ab2796565fd3c9dbfca132426935efba4ce2
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-11 21:18:51 +0300

    feat: route paths to countries

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CountriesList } from './features/countries/pages/countries-list/countries-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/countries', pathMatch: 'full' },
+  { path: 'countries', component: CountriesList },
+];
