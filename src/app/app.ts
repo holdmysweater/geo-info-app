7276c1ad237cc8ab106e777a5cb39dd6c82e8b1e
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-11 23:17:01 +0300

    feat: import Header in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TuiCardLarge } from '@taiga-ui/layout';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, TuiCardLarge],
+  imports: [RouterOutlet, TuiRoot, TuiCardLarge, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
