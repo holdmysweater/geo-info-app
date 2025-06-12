adf53e360748623d2e75feeb0b2974ce781d379c
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-12 16:12:28 +0300

    feat: import SearchBar in countries-list.ts

--- src/app/features/countries/pages/countries-list/countries-list.ts
+++ src/app/features/countries/pages/countries-list/countries-list.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SearchBar } from '../../../../shared/components/search-bar/search-bar';
 
 @Component({
   selector: 'app-countries-list',
-  imports: [],
+  imports: [
+    SearchBar
+  ],
   templateUrl: './countries-list.html',
   styleUrl: './countries-list.css'
 })
