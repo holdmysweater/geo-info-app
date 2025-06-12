da80bb7a2a51d5a181b2b587349fd75dc91f413d
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-12 19:17:34 +0300

    feat: imports for TuiTabs in nav-tabs.ts

--- src/app/shared/components/nav-tabs/nav-tabs.ts
+++ src/app/shared/components/nav-tabs/nav-tabs.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { TuiTab, TuiTabsHorizontal } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-nav-tabs',
-  imports: [],
+  imports: [
+    TuiTabsHorizontal,
+    TuiTab
+  ],
   templateUrl: './nav-tabs.html',
   styleUrl: './nav-tabs.css'
 })
 export class NavTabs {
+  protected readonly options: string[] = ['countries', 'cities'];
 
+  protected activeItemIndex: number = 0;
+
+  protected onClick(item: string): void {
+    console.log('nav-tabs.ts: clicked on \"' + item + '\"');
+  }
 }
