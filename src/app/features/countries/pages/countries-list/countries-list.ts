6fecdc5e7d1982742a5fc6fc7be85c12fffc50c2
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-12 18:18:11 +0300

    feat: import NavTabs in countries-list.ts

--- src/app/features/countries/pages/countries-list/countries-list.ts
+++ src/app/features/countries/pages/countries-list/countries-list.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBar } from '../../../../shared/components/search-bar/search-bar';
+import { NavTabs } from '../../../../shared/components/nav-tabs/nav-tabs';
 
 @Component({
   selector: 'app-countries-list',
   imports: [
-    SearchBar
+    SearchBar,
+    NavTabs
   ],
   templateUrl: './countries-list.html',
   styleUrl: './countries-list.css'
