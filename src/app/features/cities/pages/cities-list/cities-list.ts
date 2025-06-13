0e01f24e206d338fce39b3ab072d69f2a44b53ae
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-13 03:39:53 +0300

    feat: import NavTabs in cities-list.ts

--- src/app/features/cities/pages/cities-list/cities-list.ts
+++ src/app/features/cities/pages/cities-list/cities-list.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavTabs } from '../../../../shared/components/nav-tabs/nav-tabs';
 
 @Component({
   selector: 'app-cities-list',
-  imports: [],
+  imports: [
+    NavTabs
+  ],
   templateUrl: './cities-list.html',
   styleUrl: './cities-list.css'
 })
