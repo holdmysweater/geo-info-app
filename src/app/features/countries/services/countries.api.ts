00bf8aec6f9bc443b280eac88395a0f6157057df
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-15 00:26:21 +0300

    feat: method getCountries in countries.api.ts

--- src/app/features/countries/services/countries.api.ts
+++ src/app/features/countries/services/countries.api.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { CountryListResponse } from "../models/country.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,22 @@ export class CountriesApi {
   private readonly baseUrl = 'http://geodb-free-service.wirefreethought.com/v1/geo/countries';
 
   constructor(private readonly http: HttpClient) {
+  }
+
+  public getCountries(
+    offset: number,
+    limit: number,
+    namePrefix: string,
+    languageCode: string,
+    sort: string
+  ): Observable<CountryListResponse> {
+    const params = new HttpParams()
+      .set('offset', offset)
+      .set('limit', limit)
+      .set('namePrefix', namePrefix)
+      .set('languageCode', languageCode)
+      .set('sort', sort);
+
+    return this.http.get<CountryListResponse>(this.baseUrl, { params });
   }
 }
