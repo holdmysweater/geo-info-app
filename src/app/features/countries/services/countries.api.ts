c1827d994d432dd788d97ebac18cb92674000834
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-15 00:08:34 +0300

    feat: field baseUrl in countries.api.ts

--- src/app/features/countries/services/countries.api.ts
+++ src/app/features/countries/services/countries.api.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CountriesApi {
+  private readonly baseUrl = 'http://geodb-free-service.wirefreethought.com/v1/geo/countries';
 
   constructor(private readonly http: HttpClient) {
   }
