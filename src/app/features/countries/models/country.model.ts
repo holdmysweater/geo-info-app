744a02f818f62cf7adcced12cc7096745a7f43c7
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-14 23:18:39 +0300

    feat: models for api response data for countries in country.model.ts

---
+++ src/app/features/countries/models/country.model.ts
@@ -0,0 +1,37 @@
+export interface CountryListLink {
+  rel: string;
+  href: string;
+}
+
+export interface CountrySummary {
+  code: string;
+  currencyCodes: string[];
+  name: string;
+  wikiDataId: string;
+}
+
+export interface CountryListMetadata {
+  currentOffset: number;
+  totalCount: number;
+}
+
+export interface CountryListResponse {
+  links: CountryListLink[];
+  data: CountrySummary[];
+  metadata: CountryListMetadata;
+}
+
+export interface CountryDetail {
+  capital: string;
+  code: string;
+  callingCode: string;
+  currencyCodes: string[];
+  flagImageUri: string;
+  name: string;
+  numRegions: number;
+  wikiDataId: string;
+}
+
+export interface CountryDetailResponse {
+  data: CountryDetail;
+}
