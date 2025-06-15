117bafe9e52dc0d3be962da0ab378f9e3ab18c18
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-15 23:16:46 +0300

    feat: import TuiTable dependencies in countries-table.ts

--- src/app/features/countries/components/countries-table/countries-table.ts
+++ src/app/features/countries/components/countries-table/countries-table.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { TuiTableDirective, TuiTableTbody, TuiTableTh } from "@taiga-ui/addon-table";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-countries-table',
-  imports: [],
+  imports: [
+    TuiTableDirective,
+    TuiTableTh,
+    TuiTableTbody,
+    FormsModule
+  ],
   templateUrl: './countries-table.html',
   styleUrl: './countries-table.css'
 })
