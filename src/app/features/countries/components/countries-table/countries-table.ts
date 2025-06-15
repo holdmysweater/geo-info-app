ce9c5780e010432c4269ae104bc40772b7e61971
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-15 18:37:51 +0300

    feat: generate component countries countries-table

---
+++ src/app/features/countries/components/countries-table/countries-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-table',
+  imports: [],
+  templateUrl: './countries-table.html',
+  styleUrl: './countries-table.css'
+})
+export class CountriesTable {
+
+}
