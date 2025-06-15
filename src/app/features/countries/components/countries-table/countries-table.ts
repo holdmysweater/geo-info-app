6c912ee32e681ebdde884107a549bb57fc9b24ef
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-16 00:39:07 +0300

    feat: import TuiIcon in countries-table.ts

--- src/app/features/countries/components/countries-table/countries-table.ts
+++ src/app/features/countries/components/countries-table/countries-table.ts
@@ -2,6 +2,7 @@ import { Component, input } from '@angular/core';
 import { TuiTableDirective, TuiTableTbody, TuiTableTd, TuiTableTh } from "@taiga-ui/addon-table";
 import { FormsModule } from '@angular/forms';
 import { CountrySummary } from '../../models/country.model';
+import { TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-countries-table',
@@ -10,7 +11,8 @@ import { CountrySummary } from '../../models/country.model';
     TuiTableTh,
     TuiTableTbody,
     FormsModule,
-    TuiTableTd
+    TuiTableTd,
+    TuiIcon
   ],
   templateUrl: './countries-table.html',
   styleUrl: './countries-table.css'
