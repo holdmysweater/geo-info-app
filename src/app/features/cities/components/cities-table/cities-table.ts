0bc60cb105050e0b205068cbb515ea5877b57814
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-16 12:21:23 +0300

    fix: remove excess import in cities-table.ts

--- src/app/features/cities/components/cities-table/cities-table.ts
+++ src/app/features/cities/components/cities-table/cities-table.ts
@@ -1,12 +1,10 @@
 import { Component, input } from '@angular/core';
-import { TuiIcon } from '@taiga-ui/core';
 import { TuiTableDirective, TuiTableTbody, TuiTableTd, TuiTableTh } from '@taiga-ui/addon-table';
 import { PopulatedPlaceSummary } from '../../models/city.model';
 
 @Component({
   selector: 'app-cities-table',
   imports: [
-    TuiIcon,
     TuiTableDirective,
     TuiTableTbody,
     TuiTableTd,
