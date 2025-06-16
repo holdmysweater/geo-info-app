45e7fa1aac75dd94163133971f1bef225602d0a5
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-16 11:36:16 +0300

    feat: logic for cities-table

--- src/app/features/cities/components/cities-table/cities-table.ts
+++ src/app/features/cities/components/cities-table/cities-table.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TuiIcon } from '@taiga-ui/core';
+import { TuiTableDirective, TuiTableTbody, TuiTableTd, TuiTableTh } from '@taiga-ui/addon-table';
+import { PopulatedPlaceSummary } from '../../models/city.model';
 
 @Component({
   selector: 'app-cities-table',
-  imports: [],
+  imports: [
+    TuiIcon,
+    TuiTableDirective,
+    TuiTableTbody,
+    TuiTableTd,
+    TuiTableTh
+  ],
   templateUrl: './cities-table.html',
   styleUrl: './cities-table.css'
 })
 export class CitiesTable {
-
+  public cities = input<PopulatedPlaceSummary[]>();
 }
