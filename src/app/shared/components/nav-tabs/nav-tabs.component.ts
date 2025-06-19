037acfd4b5ea8ef31c56ca8b45b07e335dcb7b82
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-19 22:12:11 +0300

    feat: translating logic for nav-tabs component

--- src/app/shared/components/nav-tabs/nav-tabs.component.ts
+++ src/app/shared/components/nav-tabs/nav-tabs.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiTab, TuiTabsHorizontal } from "@taiga-ui/kit";
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { translateSignal } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-nav-tabs',
@@ -16,7 +17,7 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 })
 export class NavTabsComponent {
   protected readonly tabs = [
-    { url: 'countries', label: 'Страны' },
-    { url: 'cities', label: 'Города' }
+    { url: 'countries', label: translateSignal('navigation.countries') },
+    { url: 'cities', label: translateSignal('navigation.cities') }
   ];
 }
