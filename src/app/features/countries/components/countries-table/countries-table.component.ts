8693e08f0430ebe4b6b1c60696db003ed5d34a57
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-19 23:34:14 +0300

    feat: move query logic from countries component to countries-table component

--- src/app/features/countries/components/countries-table/countries-table.component.ts
+++ src/app/features/countries/components/countries-table/countries-table.component.ts
@@ -1,10 +1,13 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, inject, input, InputSignal, Signal } from '@angular/core';
 import { TuiTableDirective, TuiTableTbody, TuiTableTd, TuiTableTh } from "@taiga-ui/addon-table";
 import { FormsModule } from '@angular/forms';
 import { CountrySummary } from '../../models/country.model';
-import { TuiIcon } from '@taiga-ui/core';
+import { TuiIcon, TuiLoader } from '@taiga-ui/core';
 import { RouterLink } from '@angular/router';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { PaginationService } from '../../../../shared/services/pagination.service';
+import { InternationalizationService } from '../../../../shared/services/internationalization.service';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-countries-table',
@@ -16,11 +19,42 @@ import { TranslocoDirective } from '@jsverse/transloco';
     TuiTableTd,
     TuiIcon,
     RouterLink,
-    TranslocoDirective
+    TranslocoDirective,
+    TuiLoader
   ],
   templateUrl: './countries-table.component.html',
   styleUrl: './countries-table.component.css'
 })
 export class CountriesTableComponent {
-  public countries = input<CountrySummary[]>();
+  public searchParameters: InputSignal<string | undefined> = input<string>();
+
+  private readonly countriesService: CountriesService = inject(CountriesService);
+  private readonly paginationService: PaginationService = inject(PaginationService);
+  private readonly internationalizationService: InternationalizationService = inject(InternationalizationService);
+
+  protected isLoading: boolean = true;
+  protected readonly countries: Signal<CountrySummary[]> = this.countriesService.countries;
+
+  constructor() {
+    // Update countries list
+    effect(() => {
+      this.isLoading = true;
+      this.countriesService.fetchPage(
+        this.paginationService.params().currentPage,
+        this.searchParameters(),
+        this.internationalizationService.language()
+      ).subscribe();
+    });
+
+    // Reset loading flag after countries update
+    effect(() => {
+      this.countries();
+      this.isLoading = false;
+    });
+
+    // Update total page count
+    effect(() => {
+      this.paginationService.updateTotalPages(this.countriesService.pageCount());
+    });
+  }
 }
--- src/app/features/countries/pages/countries/countries.component.ts
+++ src/app/features/countries/pages/countries/countries.component.ts
@@ -1,12 +1,10 @@
-import { Component, computed, effect, inject, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { CountriesTableComponent } from '../../components/countries-table/countries-table.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TuiLoader, TuiTextfieldComponent, TuiTextfieldDirective, TuiTextfieldOptionsDirective } from '@taiga-ui/core';
+import { TuiTextfieldComponent, TuiTextfieldDirective, TuiTextfieldOptionsDirective } from '@taiga-ui/core';
 import { PaginationService } from '../../../../shared/services/pagination.service';
 import { PaginationComponent } from '../../../../shared/components/pagination/pagination.component';
-import { CountrySummary } from '../../models/country.model';
-import { InternationalizationService } from '../../../../shared/services/internationalization.service';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
@@ -19,7 +17,6 @@ import { TranslocoDirective } from '@jsverse/transloco';
     TuiTextfieldOptionsDirective,
     ReactiveFormsModule,
     PaginationComponent,
-    TuiLoader,
     TranslocoDirective
   ],
   templateUrl: './countries.component.html',
@@ -27,41 +24,7 @@ import { TranslocoDirective } from '@jsverse/transloco';
   providers: [CountriesService, PaginationService]
 })
 export class CountriesComponent {
-  private readonly service: CountriesService = inject(CountriesService);
-  private readonly paginationService: PaginationService = inject(PaginationService);
-  private readonly internationalizationService: InternationalizationService = inject(InternationalizationService);
-
-  protected isLoading: boolean = true;
-  protected readonly countries: Signal<CountrySummary[]> = this.service.countries;
-
-  protected readonly totalPageCount: Signal<number> = this.service.pageCount;
-  protected readonly currentPageIndex: Signal<number> = computed(() =>
-    this.paginationService.params().currentPage
-  );
-
   protected readonly searchBarInput: WritableSignal<string> = signal('');
-
-  constructor() {
-    // Update countries list
-    effect(() => {
-      this.isLoading = true;
-      this.service.fetchPage(
-        this.currentPageIndex(),
-        this.searchBarInput(),
-        this.internationalizationService.language()
-      ).subscribe();
-    });
-
-    effect(() => {
-      this.countries();
-      this.isLoading = false;
-    });
-
-    // Update total page count for pagination component
-    effect(() => {
-      this.paginationService.updateTotalPages(this.totalPageCount());
-    });
-  }
 
   protected onSearchBarInputChange(text: string) {
     console.log('countries.component.ts: new search bar input = \"' + (text ?? '') + '\"');
