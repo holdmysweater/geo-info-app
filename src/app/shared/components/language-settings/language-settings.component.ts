cfab1c885f2ef85878c608482b6fb10d2795be11
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-19 22:11:03 +0300

    feat: language-settings component uses internationalization service

--- src/app/shared/components/language-settings/language-settings.component.ts
+++ src/app/shared/components/language-settings/language-settings.component.ts
@@ -1,24 +1,35 @@
-import { Component, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, effect, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TuiRadioList } from '@taiga-ui/kit';
+import { InternationalizationService } from '../../services/internationalization.service';
 
 @Component({
   selector: 'app-language-settings',
   imports: [
     FormsModule,
-    TuiRadioList
+    TuiRadioList,
+    ReactiveFormsModule
   ],
   templateUrl: './language-settings.component.html',
   styleUrl: './language-settings.component.css'
 })
 export class LanguageSettings {
-  public onLanguageChange = output<string>();
+  private readonly service: InternationalizationService = inject(InternationalizationService);
 
-  protected readonly options: string[] = ['RU', 'EN'];
-  protected language: string = this.options[0]!;
+  protected readonly languageFormControl = new FormControl('');
+  protected readonly options: readonly string[] = this.service.getOptions();
+
+  private readonly language = this.service.language;
+
+  constructor() {
+    effect(() => {
+      this.languageFormControl.setValue(this.language());
+    });
+  }
 
   protected onValueChanged(): void {
-    console.log('language-settings.component.ts: changed value to \"' + this.language + '\"');
-    this.onLanguageChange.emit(this.language);
+    console.log('language-settings.component.ts: changed value to \"' + this.languageFormControl.value + '\"');
+
+    this.service.setLanguage(this.languageFormControl.value ?? '');
   }
 }
