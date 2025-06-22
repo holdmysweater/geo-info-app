4cff76eb3c99bde04ee0db65ce9f8169f61a8707
Elizaveta Lim <eliza.volga21@gmail.com>
2025-06-22 21:28:18 +0300

    fix: styles for app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,14 @@
 import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TuiCardLarge } from '@taiga-ui/layout';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { NavTabsComponent } from './shared/components/nav-tabs/nav-tabs.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, TuiCardLarge, HeaderComponent, NavTabsComponent],
+  imports: [RouterOutlet, TuiRoot, HeaderComponent, NavTabsComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.less',
 })
 export class App {
 
